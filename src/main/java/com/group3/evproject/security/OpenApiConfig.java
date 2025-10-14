eb3bc2fab9831bb5b18a7ea744ca95745d155466
dkhoa <dkhoa645@gmail.com>
2025-10-14 08:36:27 +0700

    Config Swagger

--- src/main/java/com/group3/evproject/security/OpenApiConfig.java
+++ src/main/java/com/group3/evproject/security/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.group3.evproject.security;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,8 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("EV Project API")
                         .version("1.0")
-                        .description("API documentation for EV Project"));
+                        .description("API documentation for EV Project"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local server"))
+                .addServersItem(new Server().url("https://ev-station-system-production.up.railway.app").description("Production server"));
     }
 }
