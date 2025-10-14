f48a4a546165c5085a7513c11f66e095a5539491
dkhoa <dkhoa645@gmail.com>
2025-10-14 14:18:21 +0700

    Update apikey

--- src/main/java/com/group3/evproject/EvprojectApplication.java
+++ src/main/java/com/group3/evproject/EvprojectApplication.java
@@ -8,6 +8,9 @@ public class EvprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvprojectApplication.class, args);
+
+        System.out.println("SENDGRID_USERNAME=" + System.getenv("SENDGRID_USERNAME"));
+        System.out.println("SENDGRID_API_KEY=" + System.getenv("SENDGRID_API_KEY"));
 	}
 
 }
