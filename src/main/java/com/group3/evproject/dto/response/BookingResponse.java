688cb0c746f3354a017fb3d5c8ce48f9a423ce7d
khoaleee <letrananhkhoa09042005@gmail.com>
2025-10-24 03:58:39 +0700

    fix: update-bookingTime

--- src/main/java/com/group3/evproject/dto/response/BookingResponse.java
+++ src/main/java/com/group3/evproject/dto/response/BookingResponse.java
@@ -1,5 +1,7 @@
 package com.group3.evproject.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -8,17 +10,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Thông tin trả về sau khi người dùng đặt chỗ thành công")
 public class BookingResponse {
-    private Integer bookingId;
-    private Long userId;
-    private String userName;
-    private Long stationId;
+
+    @Schema(description = "ID của lượt đặt chỗ", example = "101")
+    private Long bookingId;
+
+    @Schema(description = "Tên trạm sạc", example = "Trạm sạc VinFast Nha Trang Center")
     private String stationName;
-    private Long spotId;
-    private String spotName;
-    private Long vehicleId;
-    private String vehicleLicensePlate;
-    private LocalDateTime startTime;
+
+    @Schema(description = "Thời gian người dùng thực hiện booking")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime bookingTime;
+
+    @Schema(description = "Thời điểm bắt đầu sạc")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timeToCharge;
+
+    @Schema(description = "Thời điểm kết thúc sạc")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endTime;
+
+    @Schema(description = "Trạng thái của booking", example = "PENDING")
     private String status;
 }
