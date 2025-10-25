8fa2eb5ee3be0053dc69b95f5009d627afc537f1
dkhoa <dkhoa645@gmail.com>
2025-10-25 20:04:11 +0700

    fix(vnpay)change return url

--- src/main/java/com/group3/evproject/vnpay/VNPayService.java
+++ src/main/java/com/group3/evproject/vnpay/VNPayService.java
@@ -28,7 +28,6 @@ public class VNPayService {
         Map<String, String> vnpParamsMap = vnPayConfig.getVNPayConfig();
         vnpParamsMap.put("vnp_Amount", String.valueOf(amount));
         vnpParamsMap.put("vnp_TxnRef",  paymentTransaction.getVnpTxnRef());
-        vnpParamsMap.put("vnp_ReturnUrl", "http://localhost:5173/success");
         if (bankCode != null && !bankCode.isEmpty()) {
             vnpParamsMap.put("vnp_BankCode", bankCode);
         }
