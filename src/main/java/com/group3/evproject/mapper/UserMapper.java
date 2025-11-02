a1e0155370991ae1d3eaa1450d273931682ebc37
dkhoa <dkhoa645@gmail.com>
2025-11-02 14:08:38 +0700

    refactor(user)ignore role

--- src/main/java/com/group3/evproject/mapper/UserMapper.java
+++ src/main/java/com/group3/evproject/mapper/UserMapper.java
@@ -5,11 +5,13 @@ import com.group3.evproject.dto.request.UserUpdateRequest;
 import com.group3.evproject.dto.response.UserResponse;
 import com.group3.evproject.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserCreationRequest userCreationRequest);
+    @Mapping(target = "roles", ignore = true)
     UserResponse toUserResponse(User user);
     void updateUserFromRequest(UserUpdateRequest request, @MappingTarget User user);
 
