8766c17ea2d6cda7a8170bfa268b0737942572fa
khoaleee <letrananhkhoa09042005@gmail.com>
2025-11-06 23:09:16 +0700

    fix:record-code

--- src/main/java/com/group3/evproject/EvprojectApplication.java
+++ src/main/java/com/group3/evproject/EvprojectApplication.java
@@ -1,7 +1,9 @@
 package com.group3.evproject;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
